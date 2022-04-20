373f538dfab3df3574c53ad164c18ecbdf13e33a
erikoenuma <koenuma.eri@gmail.com>
2022-04-20 13:47:04 +0900

    first commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,9 @@ class TasksController < ApplicationController
 
   # GET /tasks
   def index
-    @tasks = Task.all
+    # @tasks = Task.all
+    @q = Task.ransack(params[:q])
+    @tasks = @q.result(distinct: true)
   end
 
   # GET /tasks/1
@@ -43,6 +45,11 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     redirect_to tasks_url, notice: 'Task was successfully destroyed.'
+  end
+
+  def search
+    index
+    render :index
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'tasks#index'
-  resources :tasks
+  resources :tasks do
+    collection do
+      match 'search' => 'tasks#search', via: [:get, :post], as: :search
+    end
+  end
 end
